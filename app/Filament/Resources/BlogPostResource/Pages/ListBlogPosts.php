3a0476eeee65d76e41a712c6ecd766b0a8ab5eb6
Your Name <you@example.com>
2025-08-01 00:55:42 +0300

    slug-tradus

--- app/Filament/Resources/BlogPostResource/Pages/ListBlogPosts.php
+++ app/Filament/Resources/BlogPostResource/Pages/ListBlogPosts.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Filament\Resources\BlogPostResource\Pages;
+namespace App\Filament\Resources\BlogPostResource\Pages; // <- THIS LINE IS CRITICAL
 
 use App\Filament\Resources\BlogPostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
-class ListBlogPosts extends ListRecords
+class ListBlogPost extends ListRecords
 {
     protected static string $resource = BlogPostResource::class;
 
