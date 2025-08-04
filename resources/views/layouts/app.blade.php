ad0ae89e52e7afe36dae168fe6f47000be9ded2a
Your Name <you@example.com>
2025-08-04 22:48:33 +0300

    ultimele retusuri

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -18,7 +17,7 @@
     <!-- Footer inclus aici -->
     @include('partials.footer')
     
-    @vite('resources/js/app.js')
-    @vite9('resources/css/app.css')
+    <!-- Linia de mai jos a fost eliminata pentru ca este incorecta si redundanta -->
+    <!-- @vite9('resources/css/app.css') -->
 </body>
 </html>
